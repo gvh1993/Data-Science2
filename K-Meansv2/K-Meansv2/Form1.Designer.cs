6c30b1e8d8d3c67fa7d77a17a6696c2a65c8e749
DESKTOP-7BMGD7J\gertj <gertjanvanh@live.nl>
2017-05-09 10:21:57 +0200

    added new k-means version (windows form)

---
+++ K-Meansv2/K-Meansv2/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace K_Meansv2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_amountOfClusters = new System.Windows.Forms.Label();
+            this.lbl_amountOfIterations = new System.Windows.Forms.Label();
+            this.txt_amountOfClusters = new System.Windows.Forms.TextBox();
+            this.txt_amountOfIterations = new System.Windows.Forms.TextBox();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.lbl_error = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.lbl_bestSSE = new System.Windows.Forms.Label();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_amountOfItemsInCluster = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_amountOfClusters
+            // 
+            this.lbl_amountOfClusters.AutoSize = true;
+            this.lbl_amountOfClusters.Location = new System.Drawing.Point(56, 100);
+            this.lbl_amountOfClusters.Name = "lbl_amountOfClusters";
+            this.lbl_amountOfClusters.Size = new System.Drawing.Size(221, 25);
+            this.lbl_amountOfClusters.TabIndex = 0;
+            this.lbl_amountOfClusters.Text = "Amount of clusters (k)";
+            // 
+            // lbl_amountOfIterations
+            // 
+            this.lbl_amountOfIterations.AutoSize = true;
+            this.lbl_amountOfIterations.Location = new System.Drawing.Point(56, 222);
+            this.lbl_amountOfIterations.Name = "lbl_amountOfIterations";
+            this.lbl_amountOfIterations.Size = new System.Drawing.Size(203, 25);
+            this.lbl_amountOfIterations.TabIndex = 1;
+            this.lbl_amountOfIterations.Text = "Amount of Iterations";
+            // 
+            // txt_amountOfClusters
+            // 
+            this.txt_amountOfClusters.Location = new System.Drawing.Point(61, 140);
+            this.txt_amountOfClusters.Name = "txt_amountOfClusters";
+            this.txt_amountOfClusters.Size = new System.Drawing.Size(100, 31);
+            this.txt_amountOfClusters.TabIndex = 2;
+            this.txt_amountOfClusters.Text = "4";
+            // 
+            // txt_amountOfIterations
+            // 
+            this.txt_amountOfIterations.Location = new System.Drawing.Point(61, 263);
+            this.txt_amountOfIterations.Name = "txt_amountOfIterations";
+            this.txt_amountOfIterations.Size = new System.Drawing.Size(100, 31);
+            this.txt_amountOfIterations.TabIndex = 3;
+            this.txt_amountOfIterations.Text = "20";
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(61, 353);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(216, 62);
+            this.btn_Start.TabIndex = 4;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // lbl_error
+            // 
+            this.lbl_error.AutoSize = true;
+            this.lbl_error.ForeColor = System.Drawing.Color.Red;
+            this.lbl_error.Location = new System.Drawing.Point(61, 456);
+            this.lbl_error.Name = "lbl_error";
+            this.lbl_error.Size = new System.Drawing.Size(0, 25);
+            this.lbl_error.TabIndex = 5;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(419, 80);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(332, 764);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // lbl_bestSSE
+            // 
+            this.lbl_bestSSE.AutoSize = true;
+            this.lbl_bestSSE.Location = new System.Drawing.Point(414, 9);
+            this.lbl_bestSSE.Name = "lbl_bestSSE";
+            this.lbl_bestSSE.Size = new System.Drawing.Size(109, 25);
+            this.lbl_bestSSE.TabIndex = 7;
+            this.lbl_bestSSE.Text = "Best SSE:";
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(810, 80);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(452, 764);
+            this.listView2.TabIndex = 8;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.List;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(805, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Selected cluster:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(414, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Clusters:";
+            // 
+            // lbl_amountOfItemsInCluster
+            // 
+            this.lbl_amountOfItemsInCluster.AutoSize = true;
+            this.lbl_amountOfItemsInCluster.Location = new System.Drawing.Point(805, 9);
+            this.lbl_amountOfItemsInCluster.Name = "lbl_amountOfItemsInCluster";
+            this.lbl_amountOfItemsInCluster.Size = new System.Drawing.Size(265, 25);
+            this.lbl_amountOfItemsInCluster.TabIndex = 11;
+            this.lbl_amountOfItemsInCluster.Text = "Amount of Items in cluster:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1397, 871);
+            this.Controls.Add(this.lbl_amountOfItemsInCluster);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.lbl_bestSSE);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.lbl_error);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.txt_amountOfIterations);
+            this.Controls.Add(this.txt_amountOfClusters);
+            this.Controls.Add(this.lbl_amountOfIterations);
+            this.Controls.Add(this.lbl_amountOfClusters);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_amountOfClusters;
+        private System.Windows.Forms.Label lbl_amountOfIterations;
+        private System.Windows.Forms.TextBox txt_amountOfClusters;
+        private System.Windows.Forms.TextBox txt_amountOfIterations;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Label lbl_error;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label lbl_bestSSE;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_amountOfItemsInCluster;
+    }
+}
+
---
+++ K-Meansv2/K-Meansv2/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace K_Meansv2
+{
+    public partial class Form1 : Form
+    { 
+        private int[][] wineMatrix;
+        private double[][] centroids;
+        private int rows;
+        private int columns;
+        private List<Cluster> clusters;
+        Random r;
+
+        int amountOfClusters, amountOfIterations;
+        
+
+        public Form1()
+        {
+            r = new Random();
+            rows = 0;
+            columns = 0;
+
+            InitializeComponent();
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            if (ValidateFields())
+            {
+                //start algorithm
+                Reset();
+                InitiateVectors();
+                InitCentroids();
+                KMeans(amountOfIterations);
+                PrintOutput();
+            }
+        }
+
+        private void Reset()
+        {
+            rows = 0;
+            columns = 0;
+            lbl_bestSSE.Text = "Best SSE:";
+        }
+
+        private bool ValidateFields()
+        {
+            lbl_error.Text = "";
+            if (!Int32.TryParse(txt_amountOfClusters.Text, out amountOfClusters))
+            {
+                lbl_error.Text += "Amount of clusters, ";
+                return false;
+            }
+            if (!Int32.TryParse(txt_amountOfIterations.Text, out amountOfIterations))
+            {
+                lbl_error.Text += "Amount of Iterations";
+                return false;
+            }
+            return true;
+        }
+
+        private void InitiateVectors()
+        {
+            List<int[]> lines;
+
+            using (var fileStream = File.OpenRead("WineData.csv"))
+            {
+                using (StreamReader reader = new StreamReader(fileStream, Encoding.Default))
+                {
+                    lines = new List<int[]>();
+                    while (!reader.EndOfStream)
+                    {
+                        int[] line = reader.ReadLine().Split(',').Select(x => Convert.ToInt32(x)).ToArray();
+                        lines.Add(line);
+                    }
+                }
+            }
+
+            //set rows and columns count
+            rows = lines.Count;
+            foreach (var vector in lines)
+            {
+                foreach (var point in vector)
+                {
+                    columns++;
+                }
+                break;
+            }
+
+            wineMatrix = new int[rows][];
+            wineMatrix = lines.ToArray();
+        }
+
+        private void InitCentroids()
+        {
+            centroids = new double[amountOfClusters][];
+            clusters = new List<Cluster>();
+
+            //random initialize centroids
+            
+            for (int i = 0; i < amountOfClusters; i++)
+            {
+                centroids[i] = new double[columns];
+                Cluster cluster = new Cluster();
+
+                for (int j = 0; j < columns; j++)
+                {
+                    //only 0 or 1
+                    centroids[i][j] = r.Next(0, 2);
+                }
+                cluster.Centroid = centroids[i];
+                clusters.Add(cluster);
+            }
+        }
+
+        //TODO CALCULATE HOW MANY CENTROIDS
+        private double CalculateDistance(int[] vector, double[] centroid)
+        {
+            double distance = 0;
+
+            // double[] powerValues = {};
+            //calculate pow and add to distance
+            for (int i = 0; i < columns; i++)
+            {
+                //powerValues[i] = Math.Pow(vector[i] - centroid[i], 2);
+                distance += Math.Pow(vector[i] - centroid[i], 2);
+            }
+
+            //calculate sqrt
+            //for (int i = 0; i < columns; i++)
+            //{
+            //    distance += powerValues[i];
+            //}
+
+            return Math.Sqrt(distance);
+        }
+
+        private void FindClosestCentroid()
+        {
+            ClearClusterWineData();
+            //iterate over winematrix vectors to calculate distance and assign to cluster/centroid
+            foreach (int[] wineVector in wineMatrix)
+            {
+                double[] closestCentroid = { };
+                double minDistanceToCentroid = Double.MaxValue;
+
+                foreach (double[] centroid in centroids)
+                {
+                    double distance = CalculateDistance(wineVector, centroid);
+
+                    if (distance < minDistanceToCentroid)
+                    {
+                        closestCentroid = centroid;
+                        minDistanceToCentroid = distance;
+                    }
+                }
+
+                //add vector to closest cluster
+                Cluster theChosenCluster = clusters.Find(x => x.Centroid == closestCentroid);
+                theChosenCluster.WineData.Add(wineVector);
+            }
+        }
+
+        private void ComputeCentroids()
+        {
+            foreach (Cluster cluster in clusters)
+            {
+                //check if wineData exists
+                if (cluster.WineData.Count == 0)
+                {
+                    continue;
+                }
+
+                //plus 
+                int[] meanCalculation = new int[columns];
+
+                foreach (int[] wineVector in cluster.WineData)
+                {
+                    meanCalculation = meanCalculation.Select((x, index) => x + wineVector[index]).ToArray();
+
+                }
+                for (int i = 0; i < meanCalculation.Length; i++)
+                {
+                    var meanCalcNumber = meanCalculation[i];
+                    double calcNumber = Convert.ToDouble(meanCalcNumber);
+
+                    cluster.Centroid[i] = calcNumber / cluster.WineData.Count;
+                }
+            }
+        }
+
+        private void ClearClusterWineData()
+        {
+            foreach (var cluster in clusters)
+            {
+                cluster.WineData = new List<int[]>();
+            }
+        }
+
+        private void PrintClusters()
+        {
+            for (int i = 0; i < clusters.Count; i++)
+            {
+                var cluster = clusters[i];
+                Console.Write("Cluster " + i);
+
+                Console.WriteLine();
+                foreach (var wineData in cluster.WineData)
+                {
+                    for (int q = 0; q < wineData.Length; q++)
+                    {
+                        Console.Write(wineData[q]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.Read();
+        }
+
+        private double SSE(List<Cluster> clusters)
+        {
+            // voor elke cluster moet je de distance van de vector naar de centroid
+            double distance = 0;
+
+            foreach (var cluster in clusters)
+            {
+                //per centroid/cluster de distance berekenen van de vectors
+                foreach (var wineData in cluster.WineData)
+                {
+                    distance += CalculateDistance(wineData, cluster.Centroid);
+                }
+            }
+            return distance;
+        }
+
+        private void KMeans(int iterations)
+        {
+            for (int i = 0; i <= iterations; i++)
+            {
+                FindClosestCentroid();
+                ComputeCentroids();
+
+                //if (i != iterations)
+                //{
+                //    //empty the winedata in the 
+                //    ClearClusterWineData();
+                //}
+                //else
+                //{
+                //    PrintClusters();
+                //}
+
+                
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                return;
+            }
+
+            listView2.Items.Clear();
+            //get cluster from cluster list by getting the number
+            string[] listViewItem = listView1.SelectedItems[0].Text.Split(' ');
+            Cluster chosenCluster = clusters[Convert.ToInt32(listViewItem[1]) - 1];
+
+            Console.WriteLine(listView1.SelectedItems[0].Text);
+            foreach (var wineDataItem in chosenCluster.WineData)
+            {
+                ListViewItem item = new ListViewItem()
+                {
+                    Text = string.Join("", wineDataItem)
+                };
+                listView2.Items.Add(item);
+
+            }
+            lbl_amountOfItemsInCluster.Text = "Amount of items in cluster: " + chosenCluster.WineData.Count;
+        }
+
+        private void PrintOutput()
+        {
+            lbl_bestSSE.Text += SSE(clusters);
+            listView1.Clear();
+
+            for (int i = 0; i < amountOfClusters; i++)
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = "cluster " + (i + 1);
+                listView1.Items.Add(item);
+            }
+        }
+    }
+}
