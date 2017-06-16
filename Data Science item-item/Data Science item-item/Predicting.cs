75ca818e8c71e74403a2f3a3f16041b24a6aeb7e
gvh1993 <gertjanvanh@live.nl>
2017-06-16 22:12:31 +0200

    removed old commented code

--- Data Science item-item/Data Science item-item/Predicting.cs
+++ Data Science item-item/Data Science item-item/Predicting.cs
@@ -19,75 +19,6 @@ namespace Data_Science_item_item
             ItemObject = itemObject;
         }
 
-        //void CalculateDeviationMatrix()
-        //{
-        //    ItemObjects = new List<ItemObject>();
-
-        //    List<int> items = new List<int>();
-        //    List<int> users = _userData.Keys.Distinct().ToList();
-
-        //    //init objects based on item
-        //    foreach (var user in _userData)
-        //    {
-        //        foreach (var rating in user.Value)
-        //        {
-        //            if (!items.Contains(rating.Key))
-        //                items.Add(rating.Key);
-
-
-        //            var deviation = ItemObjects.Find(x => x.Item == rating.Key);
-        //            if (deviation == null)
-        //            {
-        //                ItemObject dev = new ItemObject {Item = rating.Key};
-        //                dev.Users.Add(user.Key, rating.Value);
-
-        //                ItemObjects.Add(dev);
-        //            }
-        //            else
-        //            {
-        //                deviation.Users.Add(user.Key, rating.Value);
-        //            }
-        //        }
-        //    }
-
-        //    items.Sort();
-        //    users.Sort();
-
-        //    //float[,] matrix = new float[users.Count, items.Count];
-
-
-        //    //COST TO MUCH !!!! O(n4)
-        //    for (int i = 0; i < ItemObjects.Count; i++)
-        //    {
-        //        for (int j = 0; j < ItemObjects.Count; j++)
-        //        {
-        //            var dev1 = ItemObjects[i];
-        //            var dev2 = ItemObjects[j];
-
-        //            float deviationsummation = 0;
-        //            float card = 0;
-
-        //            //get the users who rated them both
-        //            foreach (var dev1User in dev1.Users)
-        //            {
-        //                foreach (var dev2User in dev2.Users)
-        //                {
-        //                    if (dev1User.Key == dev2User.Key)
-        //                    {
-        //                        deviationsummation += dev1User.Value - dev2User.Value;
-        //                        card++;
-        //                    }
-        //                }
-        //            }
-
-        //            var deviation = (deviationsummation/card);
-                    
-        //           //dev1.Deviations.Add(dev2.Item, deviation);
-        //           dev1.DeviationObjects.Add(new DeviationObject() {OtherItemId = dev2.Item, Deviation = deviation, Card = card}); 
-        //        }
-        //    }
-        //}
-
         public float Predict()
         {
             float numerator = 0;
@@ -97,10 +28,9 @@ namespace Data_Science_item_item
             {
                 var deviation = ItemObject.DeviationObjects.Find(x => x.OtherItemId == rating.Key);
                 if (Single.IsNaN(deviation.Deviation))
-                {
                     //NaN happends with deviation calulation if there are no similar raters! so we skip these in the prediction calculation
                     continue;
-                }
+                
 
                 numerator += (rating.Value + deviation.Deviation)*deviation.Card;
                 denominator += deviation.Card;
